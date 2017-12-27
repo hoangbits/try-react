70898be74cc1a581839b8667de9ba42724592024
Lê Gia Hoàng <giahoangth@gmail.com>
2017-12-27 16:43:56 +0700

    rendering a component,elements can also represent user-defined components

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function tick() {
-    const element = <h2>It is {new Date().toLocaleTimeString()}</h2>;
-    ReactDOM.render(element, document.getElementById('root'));
+function SayHi(props) {
+    return <h1>Hello, {props.name}</h1>;
 }
+const element = <SayHi name="Mason" />
+ReactDOM.render(element, document.getElementById('root'));
 
-setInterval(tick, 1000);
+
+
 registerServiceWorker();
