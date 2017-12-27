eeef4186936ab1124db5ebcae93d01b52505e496
Lê Gia Hoàng <giahoangth@gmail.com>
2017-12-27 16:55:07 +0700

    Composing Components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function SayHi(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
-const element = <SayHi name="Mason" />
-ReactDOM.render(element, document.getElementById('root'));
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
