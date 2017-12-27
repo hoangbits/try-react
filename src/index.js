aa1a382bb6081de794133f97798ec5b5816df4a0
Lê Gia Hoàng <giahoangth@gmail.com>
2017-12-27 15:55:04 +0700

    update the rendered element

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const element = <h2>this is a h2 element</h2>;
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, Mason</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
 
-ReactDOM.render(element, document.getElementById('root'));
+setInterval(tick, 1000);
 registerServiceWorker();
