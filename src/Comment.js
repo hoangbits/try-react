1f7e3493f2eb60bd64209da76508dd72db14ce48
Lê Gia Hoàng <giahoangth@gmail.com>
2017-12-27 17:41:32 +0700

    Comment component before extract

---
+++ src/Comment.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Comment from './Comment';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl:
+            'http://placekitten.com/g/64/64',
+
+    }
+}
+ReactDOM.render(<Comment date={comment.date} author={comment.author} text={comment.text} />, document.getElementById('root'));
 
 registerServiceWorker();
