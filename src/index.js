b794fca1be9e540c7350cfae30a546d32651e14c
Lê Gia Hoàng <giahoangth@gmail.com>
2017-12-27 15:39:05 +0700

    rendering an element to dom

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element = <h2>this is a h2 element</h2>;
+
+ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
