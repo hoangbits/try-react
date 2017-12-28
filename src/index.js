2be1dfe6eda8f8a806e25212917e7b2889026072
Lê Gia Hoàng <giahoangth@gmail.com>
2017-12-28 14:46:29 +0700

    converting a Function to a Class

--- src/index.js
+++ src/index.js
@@ -3,23 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Comment from './Comment';
 import registerServiceWorker from './registerServiceWorker';
+registerServiceWorker();
 
-function Clock(props) {
-    return (
-        <div>
-            <h1>Hi, manson</h1>
-            <h2>It is {props.date.toLocaleTimeString()}.</h2>
-        </div>
-    );
+
+// function Clock(props) {
+//     return (
+//         <div>
+//             <h1>Hi, Hoang</h1>
+//             <h2>It is {props.date.toLocaleTimeString()}</h2>
+//         </div>
+//     );
+// }
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi, Hoang</h1>
+                <h2>It is {this.props.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
 function tick() {
     ReactDOM.render(
-        <Clock date={new Date()} />,
+        <Clock date={new Date()} />
+        ,
         document.getElementById('root')
     );
 }
 
-setInterval(tick, 1000);
 
-registerServiceWorker();
+setInterval(tick, 1000);
