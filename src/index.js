f7541fb203389ff2a41a6c273d6a44835585f658
Lê Gia Hoàng <giahoangth@gmail.com>
2017-12-28 15:16:24 +0700

    converting a function to a class

--- src/index.js
+++ src/index.js
@@ -5,16 +5,6 @@ import Comment from './Comment';
 import registerServiceWorker from './registerServiceWorker';
 registerServiceWorker();
 
-
-// function Clock(props) {
-//     return (
-//         <div>
-//             <h1>Hi, Hoang</h1>
-//             <h2>It is {props.date.toLocaleTimeString()}</h2>
-//         </div>
-//     );
-// }
-
 class Clock extends React.Component {
     render() {
         return (
